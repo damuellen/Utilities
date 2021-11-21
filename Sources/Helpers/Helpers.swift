45926aeb0110980a5d1959e2b02716020c91e170
Daniel Müllenborn <damuellen@aol.com>
2021-11-21 15:02:21 +0100

    Update Helpers.swift

--- Sources/Helpers/Helpers.swift
+++ Sources/Helpers/Helpers.swift
@@ -76,8 +76,9 @@ extension URL {
   }
 }
 
-@_alwaysEmitIntoClient
+
 extension Collection where Self.Iterator.Element: RandomAccessCollection {
+  @_alwaysEmitIntoClient
   public func transposed() -> [[Self.Iterator.Element.Iterator.Element]] {
     guard let firstRow = self.first else { return [] }
     return firstRow.indices.map { index in self.map { $0[index] } }
