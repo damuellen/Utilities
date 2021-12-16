3f3ece812169a4a85a55ac5e1546b850d998abbf
Daniel Müllenborn <damuellen@aol.com>
2021-12-16 16:37:07 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,9 @@
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.4
 import PackageDescription
 
 let package = Package(
     name: "Utilities",
-    platforms: [.macOS(.v10_15), .iOS(.v12)],
+    platforms: [.macOS(.v10_15), .iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
