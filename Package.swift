42f7654cf1aebe5cd143f7f2d82481b36a63f35b
Daniel Müllenborn <damuellen@aol.com>
2021-12-16 17:33:02 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,28 +2,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "Utilities",
-    platforms: [.macOS(.v10_15), .iOS(.v15)],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Utilities",
-            targets: ["Utilities"]),
-    ],
-    dependencies: [
-      .package(url: "https://github.com/apple/swift-numerics.git", from: "1.0.0")
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "Utilities", dependencies: ["Helpers", "Physics"]),
-        .target(name: "Libc"),
-        .target(name: "Helpers",
-          dependencies: ["Libc", .product(name: "Numerics", package: "swift-numerics")]),
-        .target(name: "Physics",
-          dependencies: ["Helpers", "CIAPWSIF97"]),
-        .target(name: "CIAPWSIF97")
-    ]
+  name: "Utilities",
+  platforms: [.macOS(.v10_15), .iOS(.v15)],
+  products: [.library(name: "Utilities", targets: ["Utilities"])],
+  dependencies: [
+    .package(url: "https://github.com/damuellen/xlsxwriter.swift.git", .branch("main")),
+    .package(url: "https://github.com/apple/swift-numerics.git", from: "1.0.0")
+  ],
+  targets: [
+    .target(name: "Libc"), 
+    .target(name: "CZLib"), 
+    .target(name: "CIAPWSIF97"),
+    .target(name: "Physics", dependencies: ["Helpers", "CIAPWSIF97"]),
+    .target(name: "Utilities", dependencies: ["Helpers", "Physics"]),
+    .target(name: "Helpers", dependencies: ["Libc", 
+      .byName(name: "CZLib", condition: .when(platforms: [.macOS, .linux])),
+      .product(name: "Cminizip", package: "xlsxwriter.swift", condition: .when(platforms: [.windows])),
+      .product(name: "Numerics", package: "swift-numerics")])
+  ]
 )
+
+#if os(Windows)
+package.targets.removeAll(where: { $0.name == "CZLib" })
+package.products.first!.linkerSettings = [.linkedLibrary("zlibstatic.lib")]
+#endif
