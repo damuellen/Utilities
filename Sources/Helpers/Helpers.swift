7bef2a31eb0171518b680ab049b38a53e8b9be1b
Daniel Müllenborn <damuellen@aol.com>
2022-01-02 00:08:47 +0100

    Update Helpers.swift

--- Sources/Helpers/Helpers.swift
+++ Sources/Helpers/Helpers.swift
@@ -37,7 +37,7 @@ public func terminalWidth() -> Int {
     if ioctl(1, UInt(TIOCGWINSZ), &ws) == 0 { cachedTerminalWidth = Int(ws.ws_col) - 1 }
   }
   #endif
-  if cachedTerminalWidth < 0 { cachedTerminalWidth = 80 }
+  if cachedTerminalWidth < 0 { cachedTerminalWidth = 120 }
   return cachedTerminalWidth
 }
 
