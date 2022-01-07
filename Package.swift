b1ffc48a20a859f8f0bb5b56d3f4ca87eed2d05f
Daniel Müllenborn <damuellen@aol.com>
2022-01-07 17:25:14 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,23 +1,15 @@
 // swift-tools-version:4.2
 import PackageDescription
 
-var dependencies: [Package.Dependency] = []
-var targetDependencies: [Target.Dependency] = ["Libc", "CZLib"]
-#if swift(>=5.4)
-dependencies = [.package(url: "https://github.com/apple/swift-numerics.git", from: "1.0.0")]
-targetDependencies.append("Numerics")
-#endif
-
 let package = Package(
   name: "Utilities",
   products: [.library(name: "Utilities", targets: ["Utilities"])],
-  dependencies: dependencies,
   targets: [
     .target(name: "Libc"), 
     .target(name: "CZLib"), 
     .target(name: "CIAPWSIF97"),
     .target(name: "Physics", dependencies: ["Helpers", "CIAPWSIF97"]),
     .target(name: "Utilities", dependencies: ["Helpers", "Physics"]),
-    .target(name: "Helpers", dependencies: targetDependencies),
+    .target(name: "Helpers", dependencies: ["Libc", "CZLib"]),
   ]
 )
