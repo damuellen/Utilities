f25ecd7c6922dd5025e6a11cf6daa92a6cf0f779
Daniel Müllenborn <damuellen@aol.com>
2022-01-12 23:41:18 +0100

    Update Gnuplot.swift

--- Sources/Helpers/Gnuplot.swift
+++ Sources/Helpers/Gnuplot.swift
@@ -457,26 +457,3 @@ extension Array where Element == String {
 extension Dictionary where Key == String, Value == String {
   var concatenated: String { self.map { "set " + $0.key + " " + $0.value + "\n" }.joined() }
 }
-
-extension Gnuplot {
-  public convenience init<T: FloatingPoint>(y1: [[T]], y2: [[T]]) { self.init(y1s: [y1], y2s: [y2]) }
-  public convenience init<S: Sequence, F: FloatingPoint>(xys: S..., labels: [String]..., titles: [String] = [], style: Style = .linePoints) where S.Element == SIMD2<F> {
-    self.init(xys: xys.map { xy in xy.map { [$0.x, $0.y] } }, xylabels: labels, titles: titles, style: style) 
-  }
-  public convenience init<S: Sequence, F: FloatingPoint>(xys: S..., labels: [String]..., titles: [String] = [], style: Style = .linePoints) where S.Element == [F] {
-    self.init(xys: xys.map { xy in xy.map { $0 } }, xylabels: labels, titles: titles, style: style)
-  }
-  public convenience init<S: Sequence, F: FloatingPoint>(xs: S..., ys: S..., labels: [String]..., titles: String..., style: Style = .linePoints) where S.Element == F {
-    self.init(xys: zip(xs, ys).map { a, b in zip(a, b).map { [$0, $1] } }, xylabels: labels, titles: titles, style: style)
-  }
-  public convenience init<S: Collection, F: FloatingPoint>(xs: S, ys: S..., labels: [String]..., titles: String..., style: Style = .linePoints) where S.Element == F {
-    let xys = xs.indices.map { index -> [F] in [xs[index]] + ys.map { $0[index] } }
-    self.init(xys: [xys], xylabels: labels, titles: titles, style: style)
-  }
-  public convenience init<S: Sequence, F: FloatingPoint>(xs: S..., labels: [String]..., titles: String..., style: Style = .linePoints) where S.Element == F {
-    self.init(xys: xs.map { $0.map { [$0] } }, xylabels: labels, titles: titles, style: style)
-  }
-  public convenience init<T: FloatingPoint>(xy1s: [[T]]..., xy2s: [[T]]..., titles: String..., style: Style = .linePoints) {
-    self.init(xy1s: xy1s, xy2s: xy2s, titles: titles, style: style)
-  }
-}
