a49c138a7e7a4e3d7a9a69e8e5d64dd73cb90a96
Daniel Müllenborn <damuellen@aol.com>
2022-01-12 23:41:49 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,11 +5,11 @@ let package = Package(
   name: "Utilities",
   products: [.library(name: "Utilities", targets: ["Utilities"])],
   targets: [
-    .target(name: "Libc"),
-    .target(name: "CZLib"),
+    .target(name: "Libc"), 
+    .target(name: "CZLib"), 
     .target(name: "CIAPWSIF97"),
     .target(name: "Physics", dependencies: ["Helpers", "CIAPWSIF97"]),
     .target(name: "Utilities", dependencies: ["Helpers", "Physics"]),
-    .target(name: "Helpers", dependencies: ["Libc", "CZLib"]),
+    .target(name: "Helpers", dependencies: ["Libc", "CZLib"], exclude: ["GnuplotInit.swift"])
   ]
 )
