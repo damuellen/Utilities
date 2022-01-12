57258808bb8e3183790f27d678b204094caf23d3
Daniel Müllenborn <damuellen@aol.com>
2022-01-12 23:43:01 +0100

    Update and rename Package@swift-5.5.swift to Package@swift-5.4.swift

--- Package@swift-5.4.swift
+++ Package@swift-5.4.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.4
 import PackageDescription
 
 let package = Package(
   name: "Utilities",
   products: [.library(name: "Utilities", targets: ["Utilities"])],
-  dependencies: [.package(url: "https://github.com/apple/swift-numerics.git", from: "1.0.0")],
+  // dependencies: [.package(url: "https://github.com/apple/swift-numerics.git", from: "1.0.0")],
   targets: [
     .target(name: "Libc"),
     .target(name: "CZLib"),
@@ -13,6 +13,6 @@ let package = Package(
     .target(name: "Utilities", dependencies: ["Helpers", "Physics"]),
     .target(
       name: "Helpers",
-      dependencies: ["Libc", "CZLib", .product(name: "Numerics", package: "swift-numerics")]),
+  //  dependencies: ["Libc", "CZLib", .product(name: "Numerics", package: "swift-numerics")]),
   ]
 )
