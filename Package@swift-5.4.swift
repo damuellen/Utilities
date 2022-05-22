5bf88cf2859d133e8fe9e25501f46299234e3334
Daniel <damuellen@aol.com>
2022-05-22 12:36:21 +0200

    ZLib linux only

--- Package@swift-5.4.swift
+++ Package@swift-5.4.swift
@@ -1,17 +1,28 @@
 // swift-tools-version:5.4
 import PackageDescription
 
+#if os(Linux)
 let package = Package(
   name: "Utilities",
   products: [.library(name: "Utilities", targets: ["Utilities"])],
-  // dependencies: [.package(url: "https://github.com/apple/swift-numerics.git", from: "1.0.0")],
   targets: [
     .target(name: "Libc"),
     .target(name: "CZLib"),
     .target(name: "Physics", dependencies: ["Helpers"]),
     .target(name: "Utilities", dependencies: ["Helpers", "Physics"]),
-    .target(
-      name: "Helpers", dependencies: ["Libc", "CZLib",
-    ])// .product(name: "Numerics", package: "swift-numerics")]),
+    .target(name: "Helpers", dependencies: [
+      "Libc", "CZLib",])
   ]
 )
+#else
+let package = Package(
+  name: "Utilities",
+  products: [.library(name: "Utilities", targets: ["Utilities"])],
+  targets: [
+    .target(name: "Libc"),
+    .target(name: "Physics", dependencies: ["Helpers"]),
+    .target(name: "Utilities", dependencies: ["Helpers", "Physics"]),
+    .target(name: "Helpers", dependencies: ["Libc"])
+  ]
+)
+#endif
