542d74df77bbf1bf5589fcd104787d0ae520dbfd
Daniel Müllenborn <damuellen@aol.com>
2022-07-10 22:59:26 +0200

    Update GnuplotInit.swift

--- Sources/Helpers/GnuplotInit.swift
+++ Sources/Helpers/GnuplotInit.swift
@@ -8,6 +8,8 @@
 //  http://www.apache.org/licenses/LICENSE-2.0
 //
 
+import Foundation
+
 extension Gnuplot {
   public convenience init<T: FloatingPoint>(y1: [[T]], y2: [[T]]) { self.init(y1s: [y1], y2s: [y2]) }
   public convenience init<S: Sequence, F: FloatingPoint>(xys: S..., labels: [String]..., titles: [String] = [], style: Style = .linePoints) where S.Element == SIMD2<F> {
@@ -28,5 +30,9 @@ extension Gnuplot {
   }
   public convenience init<T: FloatingPoint>(xy1s: [[T]]..., xy2s: [[T]]..., titles: String..., style: Style = .linePoints) {
     self.init(xy1s: xy1s, xy2s: xy2s, titles: titles, style: style)
+  }
+  @available(macOS 10.12, *)
+  public convenience init<S: Sequence, F: FloatingPoint>(y1s: S..., y2s: S..., titles: [String] = [], range: DateInterval) where S.Element == F {
+    self.init(y1s: y1s.map(Array.init), y2s: y2s.map(Array.init), titles: titles, range: range)
   }
 }
