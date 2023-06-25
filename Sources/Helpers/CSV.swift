dcc1ea85d0413b54c939ddbbf48ec19f26b92a22
Daniel Müllenborn <damuellen@aol.com>
2023-06-25 17:12:24 +0200

    Update CSV.swift

--- Sources/Helpers/CSV.swift
+++ Sources/Helpers/CSV.swift
@@ -71,7 +71,7 @@ public struct CSVReader {
       }
     }
   }
-
+  @available(macOS 10.15.4, iOS 13, watchOS 6, tvOS 13, *)
   public init?(atPath: String, separator: Unicode.Scalar = ",", filter: String..., skip: String..., dateColumn: Int? = nil) {
     let fileHandle = FileHandle(forReadingAtPath: atPath)
     let data = try? fileHandle?.readToEnd()
